0b44165d9a6f230e5ccbe4b37621c99f10f1dc35
mariel-delagarza <mariel.code@gmail.com>
2020-01-29 23:15:01 -0500

    Delete this.state

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 
 const ResourceList = () => {
   const [resources, setResources] = useState([]);
-  
+
   const fetchResource = async () => {
     const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
 
     this.setState({ resources: response.data });
   }
 
-  return <div>{this.state.resources.length}</div>;
+  return <div>{resources.length}</div>;
 }
 
 export default ResourceList;
