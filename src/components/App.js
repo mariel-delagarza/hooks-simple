7e9c520605f83e157b004de61f915dd71963de2f
mariel-delagarza <mariel.code@gmail.com>
2020-01-29 22:32:19 -0500

    Finish refactor

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,10 @@ const App = () => {
   return (
     <div>
       <div>
-        <button onClick={() => this.setState({resource: 'posts'})}>Posts</button>
-        <button onClick={() => this.setState({resource: 'todos'})}>Todos</button>
+        <button onClick={() => setResource('posts')}>Posts</button>
+        <button onClick={() => setResource('todos')}>Todos</button>
       </div>
-      {this.state.resource}
+      {resource}
     </div>
   );
 }
