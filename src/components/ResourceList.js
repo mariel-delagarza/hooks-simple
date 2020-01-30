9fa9638ecdae4a3747f605597615f9ffba830ef7
mariel-delagarza <mariel.code@gmail.com>
2020-01-29 23:15:51 -0500

    destructure resource from props

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ResourceList = () => {
+const ResourceList = ({ resource }) => {
   const [resources, setResources] = useState([]);
 
   const fetchResource = async () => {
