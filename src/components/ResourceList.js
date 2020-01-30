b7ec65a2a71f6773911849d3acd16e98111d5309
mariel-delagarza <mariel.code@gmail.com>
2020-01-29 22:58:57 -0500

    Make request to axios

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -2,6 +2,10 @@ import React from 'react';
 import axios from 'axios';
 
 class ResourceList extends React.Component {
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+  }
+
   render() {
     return <div>{this.props.resource}</div>;
   }
