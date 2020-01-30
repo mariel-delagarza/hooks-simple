22191dfd9659e997dfdf0ff7b9df372a4fa1b63c
mariel-delagarza <mariel.code@gmail.com>
2020-01-29 23:31:55 -0500

    refactor to define and immediately invoke function

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -4,17 +4,18 @@ import axios from 'axios';
 const ResourceList = ({ resource }) => {
   const [resources, setResources] = useState([]);
 
-  const fetchResource = async (resource) => {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+  useEffect(
+    () => {
+      (async resource => {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
 
-    setResources(response.data);
-  }
-
-  useEffect(() => {
-    fetchResource(resource);
-  }, [resource]);
+        setResources(response.data);
+      })(resource);
+    }, 
+    [resource]
+  );
 
   return <div>{resources.length}</div>;
-}
+};
 
 export default ResourceList;
